40967fdb5d60eaf25c10450cfcab764830767587
erikstannemark <erikstannemark@gmail.com>
2021-02-05 09:55:24 +0100

    created an increment method

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        int increment = increment(num);
+
+        System.out.println(increment);
+
+    }
+
+        public static int increment(int number) {
+
+            return ++number;
+        }
+
+}
